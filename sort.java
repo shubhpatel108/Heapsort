52ea982e61cb0db525f6f0ab4a1dd779a2302348
Shubham <shubhpatel108@gmail.com>
2013-09-29 19:07:21 +0530

    Modify sort to sort in ascending order

--- sort.java
+++ sort.java
@@ -7,14 +7,14 @@ public int [] heapify(int[] heap, int i, int mode)
 		return heap;
 	if (2*i +1 < heap.length-mode)
 	{	
-		if(2*i +2 < heap.length-mode && heap[2*i+2]<heap[i] && heap[2*i+2]<heap[2*i+1])
+		if(2*i +2 < heap.length-mode && heap[2*i+2]>heap[i] && heap[2*i+2]>heap[2*i+1])
 		{
 			temp = heap[2*i+2];
 			heap[2*i+2] = heap[i];
 			heap[i] = temp;
 			heap = heapify(heap, 2*i+2, mode);
 		}
-		else if(heap[2*i+1]<heap[i])
+		else if(heap[2*i+1]>heap[i])
 		{
 			temp = heap[2*i+1];
 			heap[2*i+1] = heap[i];
